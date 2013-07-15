fc22a41386244a13a8d017503b4dea3df78a615f
Manuel Muiña <manuerux@gmail.com>
2013-07-15 11:00:27 +0200

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -8,9 +9,33 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.implicitly_wait(3)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
+		# The user has heard about a cool new online to-do app. She goes
+        # to check out its homepage
 		self.browser.get('http://localhost:8000')
 
+		#She notices the page title and header mention to-do lists
 		self.assertIn('To-Do',self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		#She notices the page title and header mention to-do lists
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list table
+		inputbox.send_keys(Keys.ENTER)
+
+
+
+		# She types "Buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+		
 		self.fail('Finish the test!')
 
 	def tearDown(self):
